26dd12fa51b10bb4300465dc87e2045e0bd9a870
liweijie <lwj19970331@gmail.com>
2018-08-16 17:10:18 +0800

    find a bug and fix it

--- src/walkerDetection_main.cpp
+++ src/walkerDetection_main.cpp
@@ -9,7 +9,7 @@ int main(int argc,char ** argv)
     char imageFileName[50];
     WalkerDetection detector;
     
-    for(int i = 0; i < 24; i++)
+    for(int i = 0; i < 45; i++)
     {
         sprintf(imageFileName,"../input/images/image_%08d_0.png",i);
         Mat src = imread(imageFileName);
